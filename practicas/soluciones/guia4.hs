6e8aae18d8335f487880899040b5a5fefb04d7e7
aitcass <132713433+aitcass@users.noreply.github.com>
2024-04-15 11:38:00 -0300

    Add files via upload

--- practicas/soluciones/guia4.hs
+++ practicas/soluciones/guia4.hs
@@ -1,26 +1,83 @@
---ejercicio 1
-fibonacci :: Integer -> Integer 
-fibonacci n | n == 0 = 0
-            | n == 1 = 1
-            | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+--Ejercicio factorial
+factorial :: Int -> Int
+factorial n | n == 0 = 1
+            | n > 0 = n * factorial (n - 1)
+    
+--Ejercicio 1
 
---ejercicio 2
+fibonacci :: Integer -> Integer
+fibonacci n 
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fibonacci(n - 1) + fibonacci(n - 2)
+
+--Ejercicio 2
 parteEntera :: Float -> Integer
---parteEntera x = x - (x - parteEntera x)
---opcion2 parteEntera x = sumar n desde 0  de 1 en 1
--- hasta que n > x y restarle 1. entonces n <= x < n + 1
-
-parteEntera x | x >= 0 = truncar x 0
-              | x < 0 = truncar (-x) 0
+parteEntera x 
+    | x >= 0 = truncar x 0
+    | x < 0 = truncar (-x) 0
     where
         truncar :: Float -> Integer -> Integer
-        truncar y n | fromIntegral n > y = n -1
-                    | otherwise = truncar y (n + 1)              
--- ¿todo esto solo para tener la variable n, no se puede hacer una variable n = 0?
+        truncar y n 
+            | fromIntegral n > n = n - 1
+            | otherwise = truncar y (n + 1)
+
+parteEntera2 :: Float -> Integer
+parteEntera2 x = truncate x 
 
 --ejercicio 3
-esDivisible :: Integer -> Integer -> Bool
---x es divisible por y  si existe un k entero : x = k * y
-esDivisible x y | x == 0 = True
-                | x < 0 = False
-                | otherwise = esDivisible (x - y) y
+esDibisible :: Integer -> Integer -> Bool
+esDibisible n1 n2
+    | n1 == 0 = True 
+    | n1 < 0 = False
+    | otherwise = esDibisible (n1 - n2) n2
+
+--Ejercicio 4
+sumaImpares :: Integer -> Integer
+sumaImpares n = contar 1 n
+    where
+        contar :: Integer -> Integer -> Integer
+        contar uno hasta
+            | hasta == 0 = 0
+            | otherwise = uno + contar (uno + 2) (hasta - 1)
+
+--Ejercicio 5
+medioFact :: Integer -> Integer
+medioFact n
+    | n == 0 = 1
+    | n == 1 = 1
+    | otherwise = n * medioFact (n - 2) 
+
+--Ejercicio 6
+sumaDigitos :: Integer ->Integer
+sumaDigitos n
+    | div n 10 <= 0 = 1
+    | otherwise = mod n 10 + sumaDigitos (div n 10)
+
+--Ejercicio 7
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n
+    | n < 10 = True
+    | mod n 10 == div (mod n 100) 10 = todosDigitosIguales (div n 100)
+    | otherwise = False
+
+-- todosDigitosIguales :: Integer -> Bool
+-- todosDigitosIguales n 
+--     | abs(n) < 10 == True
+--     | (ultimoDigito n == anteultimoDigito n) = todosDigitosIguales (div n 10)
+--     | otherwise = False
+
+-- ultimoDigito :: Integer -> Integer
+-- ultimoDigito x = mod x 10
+
+-- anteultimoDigito :: Integer -> Integer
+-- anteultimoDigito y = mod (div y 10) 10
+
+--Ejercicio 8
+iesimoDigito :: Integer ->Integer ->Integer
+iesimoDigito n i = mod (div n (10 ^ ((cantDigitos n) - i))) 10 
+    where 
+        cantDigitos :: Integer ->Integer
+        cantDigitos n
+            | div n 10 <= 0 = 1
+            | otherwise = 1 + cantDigitos (div n 10)
