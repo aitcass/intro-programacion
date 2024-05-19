0008e5834554be35b6a7b64784babfe47ae44ea4
aitor <132713433+aitorcastro@users.noreply.github.com>
2024-05-19 00:12:09 -0300

    Add files via upload

---
+++ practicas/soluciones/guia6.py
@@ -0,0 +1,8 @@
+from importlib import reload 
+#ejercicio 1
+#1.1
+def imprimir_hola_mundo():
+  print("Hello World!")
+#1.2
+def imprimir_un_verso():
+  print("Strangers in the night\nTwo lonely people we were strangers in the night\nUp to the moment\nWhen we said our first hello\nLittle did we know\nstrangers in the night by Frank f* Sinatra") 
